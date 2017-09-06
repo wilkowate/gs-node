8d760515a49715dd24c20b496165becc785fec04
ola <ola@thinkpad>
2017-09-06 18:31:09 +0200

    /loadMapTypes added

--- controllers/docProcessor.js
+++ controllers/docProcessor.js
@@ -5,7 +5,14 @@ var express = require('express')
   router.get('/loadDocTypes',  function(req, res) {
 	  var id =1;
 	  //console.log('loadDocTypesloadDocTypesloadDocTypes');
-	  DocTypeAttr.get(id, function (err, layers) {
+	  DocTypeAttr.get("Document", function (err, layers) {
+		  res.send(layers);
+	  })
+	})
+	
+	router.get('/loadMapTypes',  function(req, res) {
+	  var id =1;
+	  DocTypeAttr.get("Map", function (err, layers) {
 		  res.send(layers);
 	  })
 	})
