2fe8a41f38a05ca855d7213bf76838c2bb3a704d
ola <ola@thinkpad>
2017-11-28 11:17:55 +0100

    ver0.5

--- models/category.js
+++ models/category.js
@@ -14,8 +14,19 @@ var parentId = 0;
 var docsCounter = '';
 var childDocsCounter = '';
 
+var key = 0;
+var title = '';
+var expanded = false;
+var folder = false;
+var children = [];
+var loaded = false;
+var data = {loaded : false};
+
 function Category(id){
 	this.id = id;
+	this.data = {loaded : false};
+	this.title = "dummy";
+	this.children = [];
 }
 
 /**
@@ -43,51 +54,147 @@ function loadCats(id,done,connection) {
 	
 	var resultData = [];
 	
+	resultCategories = new Map();
+	
+	var catIds = [];
+	
+	var query = "SELECT * FROM Category where parent = "+id;
+	
+	request = new Request(query, function(err, rowCount) {
+	    if (err) {
+	      console.log(err);
+	    } else {
+	    	
+		    //var res1 ={
+		    //		"categories":resultData
+		    //	  };
+		    
+		    console.log(JSON.stringify(resultData));
+		    loadChildren( connection, done, resultCategories,catIds);
+		    
+		   // done(null,resultData);
+	      //console.log(rowCount + ' rows');
+	    }
+	   // connection.close();
+	});
+	  
+	request.on('row', function(columns) {
+		var categoryNode = new Category(1);
+	    columns.forEach(function(column) {
+	    	if (column.value === null) {
+
+	      } else if(column.metadata.colName == 'Label'){
+	    	  categoryNode.title = column.value;
+	      } else if(column.metadata.colName == 'Category'){
+	    	  categoryNode.id = column.value;
+	    	  categoryNode.key = column.value;
+	      } else if(column.metadata.colName == 'Parent'){
+	    	  categoryNode.parent = column.value;
+	      } else if(column.metadata.colName == 'DocsCounter'){
+	    	  categoryNode.docsCounter = column.value;
+	      } else if(column.metadata.colName == 'ChildDocsCounter'){
+	    	  categoryNode.childDocsCounter = column.value;
+	      }
+	    });
+	    categoryNode.children = [];
+	   // categoryNode.data1 = false;
+	    categoryNode.data.loaded = true;
+	    categoryNode.folder = true;
+	    categoryNode.icon = "images/icons/layer-layer-on.png";
+		  categoryNode.lazy = true;
+	    console.log('-------- load'+categoryNode.id);
+	    catIds.push(categoryNode.id);
+	    resultData.push(categoryNode);
+	    resultCategories.set(categoryNode.id, categoryNode);
+	});
+	  
+	request.on('done', function (rowCount, more, rows) { 
+		console.log('DONE');
+	});
+
+	connection.execSql(request);
+}
+
+/**
+ * 
+ * @param id
+ * @param connection
+ * @param resultCategories
+ */
+function loadChildren( connection,done ,resultCategories, catIds ){
+	
+	if(catIds.length == 0){
+		var res = [];
+		
+		resultCategories.forEach(function (value, key, map) { 
+			res.push(value);
+		});
+		
+		//console.log("the end"+JSON.stringify([...resultCategories]));//JSON.stringify(resultCategories));
+		done(null,JSON.stringify(res));
+		return;
+		
+	}
+	
+	var id = catIds[0];
+	catIds.shift();
+	var categoryParent = resultCategories.get(id);
+	console.log(catIds+'   DONloadChildrenloadChildrenloadChildrenloadChildrenE    '+id);
+	
 	var query = "SELECT * FROM Category where parent = "+id;
 	
 	  request = new Request(query, function(err, rowCount) {
 	    if (err) {
 	      console.log(err);
 	    } else {
-	    	
-		    var res1 ={
-		    		"categories":resultData
-		    	  };
-		    
-		    console.log(JSON.stringify(resultData));
-		    
-		    done(null,res1);
-	      console.log(rowCount + ' rows');
+  
+		    console.log("rrrrID "+id);
+		    loadChildren( connection,done, resultCategories, catIds);
 	    }
-	    connection.close();
+	   // connection.close();
 	  });
-
+	  
 	  request.on('row', function(columns) {
-		  var doc = new Category(1);
-	    columns.forEach(function(column) {
+		  var categoryNode = new Category(1);
+		  columns.forEach(function(column) {
 	      if (column.value === null) {
 
 	      } else if(column.metadata.colName == 'Label'){
-	    	  doc.label = column.value;
+	    	  categoryNode.title = column.value;
 	      } else if(column.metadata.colName == 'Category'){
-	    	  doc.id = column.value;
+	    	  categoryNode.id = column.value;
+	    	  categoryNode.key = column.value;
 	      } else if(column.metadata.colName == 'Parent'){
-	    	  doc.parent = column.value;
+	    	  categoryNode.parent = column.value;
 	      } else if(column.metadata.colName == 'DocsCounter'){
-	    	  doc.docsCounter = column.value;
+	    	  categoryNode.docsCounter = column.value;
 	      } else if(column.metadata.colName == 'ChildDocsCounter'){
-	    	  doc.childDocsCounter = column.value;
+	    	  categoryNode.childDocsCounter = column.value;
+	      } else if(column.metadata.colName == 'EndNode'){
+	    	  if(column.value == 1){
+	    		  categoryNode.folder = false;
+	    		  categoryNode.lazy = false;
+	    	  } else {
+	    		  categoryNode.folder = true;
+	    		  categoryNode.lazy = true;
+	    		  categoryNode.children = null;
+	    		  
+	    		  
+	    		 // var cat = new Category(1);
+	    		  //categoryNode.children.push(cat);
+	    	  }
 	      }
 	      
 	      
 	    });
-	    resultData.push(doc);
+	    
+	    console.log('-------- children '+categoryNode.id);
+	    categoryNode.data.loaded = false;
+	    categoryParent.children.push(categoryNode);
+	    
 
 	  });
 	  
-	  request.on('done', function (rowCount, more, rows) { 
-		  console.log('DONE');
-	  });
+	  connection.execSql(request);
 
-	connection.execSql(request);
 }
